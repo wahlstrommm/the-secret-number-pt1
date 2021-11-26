7c61a8f2a07dd047ffe7ed372690ba299754c450
Magnus Vahlström <Wahlstrommm@gmail.com>
2021-11-27 00:00:01 +0100

    Super simple assigment 3part here is the 1 part

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Det_hemliga_talet__del_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomerare = new Random(); //random randomerare
+            int slump_tal = randomerare.Next(1, 101); // detta ger oss ett tal mellan 1-100 ett slumpmässigt sådant.
+            Console.WriteLine(slump_tal); // skriver ut det slumpmässiga talet.
+            Console.ReadKey();//så progammet inte stängs ner.
+        }
+    }
+}
